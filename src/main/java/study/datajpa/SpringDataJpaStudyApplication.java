6af768b90e702afd327aa4f2e7199e4c78e428fd
soonmyeong <smkwon@neonexsoft.com>
2022-01-15 21:32:51 +0900

    init settings

---
+++ src/main/java/study/datajpa/SpringDataJpaStudyApplication.java
@@ -0,0 +1,13 @@
+package study.datajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/datajpa/SpringDataJpaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package study.datajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
