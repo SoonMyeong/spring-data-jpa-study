d9990cc19928d9eb43ca64d1670661eba42826e6
soon <soon_myeong@naver.com>
2022-01-23 22:35:53 +0900

    @query 테스트..

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -80,5 +80,22 @@ class MemberRepositoryTest {
         assertThat(result.size()).isEqualTo(1);
     }
 
+    @Test
+    @DisplayName("@query 테스트")
+    public void testQuery() {
+        Member m1 = new Member("AAA",10);
+        Member m2 = new Member("AAA",20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findUser("AAA",20);
+
+        assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+        assertThat(result.get(0).getAge()).isEqualTo(20);
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+
+
 
 }
